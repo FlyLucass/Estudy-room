6b05ad54402a0971a7f3750efb487a62c69369df
Lucas <lucassantos30957@gmail.com>
2025-02-18 09:02:12 -0300

    interatividade com DOCJS

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,7 @@
-function calculando() {
+//@ts-check
 
+
+function calculando() {
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
     const op = document.getElementById("op").value;
@@ -18,7 +20,9 @@ function calculando() {
         case '/':
             valor = num1 / num2;
             break;
-
+        case '%':
+            valor = num1 % num2;
+            break;
         default: valor = "siga os padroes estabelecidos";
     }
 
