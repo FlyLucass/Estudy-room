b4afbfd18eabfe2e0c1d144a84fa5081a2eb5cae
Lucas <lucassantos30957@gmail.com>
2025-02-18 08:58:41 -0300

    iniciando um novo curso, focado em javascript

---
+++ javascript/javascript_curso/estruturaderepeticao.js
@@ -0,0 +1,19 @@
+// console.log("contador:")
+// for (let i = 1; i < 11; i++) {
+//     console.log(i)
+// }
+
+// let i = 0
+// while (i < 10) {
+//     i += 1
+//     console.log(i)
+// }
+
+// let i = 10;
+// do {
+//     console.log(i);
+//     i -= 1
+
+// } while (i >= 0);
+
+// While == enquanto
