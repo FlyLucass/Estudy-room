95e9dd516e7f73a1879e21f03e57cbd771b8ce33
Lucas <lucassantos30957@gmail.com>
2025-03-15 21:09:25 -0300

    FIX:calculadora melhorada prontinho para uso, próximo passo:fazer rodar com node

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,13 @@
 
-
-
 function calculando() {
-    const num1 = document.getElementById("num1").value;
-    const num2 = document.getElementById("num2").value;
+    const num1 = Number(document.getElementById("num1").value);
+    const num2 = Number(document.getElementById("num2").value);
     const op = document.getElementById("op").value;
     let valor;
 
     switch (op) {
         case '+':
-            valor = +num1 + +num2;
+            valor = num1 + +num2;
             break;
         case '-':
             valor = num1 - num2;
