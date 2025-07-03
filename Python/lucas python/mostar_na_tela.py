cad34bd279ba83656fba3fd8a83a67e9a03d83c5
Lucas Santos <lucassantos30957@gmail.com>
2025-07-03 07:17:09 -0300

    feat: nova chamada atribuida

--- Python/lucas python/mostar_na_tela.py
+++ Python/lucas python/mostar_na_tela.py
@@ -1,14 +1,20 @@
 from UtilidadesCeV import moeda
-
+from UtilidadesCeV import moeda_value
 
 print(moeda.prog())
 
-valor_user=float(input("Digite um valor "))
-valor_em_porcetagem=float(input("Digite um valor em porcetagem "))
+# valor_user=float(input("Digite um valor "))
+# valor_em_porcetagem=float(input("Digite um valor em porcetagem "))
 
 # print("O valor {} \nAumentou {} ".format(moeda.moeda(valor_user),moeda.aumentar(valor_user,valor_em_porcetagem,True)))
 # print(f"Diminuiu {moeda.diminuir(valor_user,valor_em_porcetagem,False)}")
 # print(f"Dobro {moeda.dobro(valor_user,True)} \nMetade {moeda.metade(valor_user,False)}" )
 
-moeda.resumo(valor_user,valor_em_porcetagem,True)
+# moeda.resumo(valor_user,valor_em_porcetagem,True)
+
+
+#entrada apenas com input
+valor_user=input("Digite um valor ")
+valor_em_porcetagem=input("Digite um valor em porcetagem ")
+moeda_value.leiadinheiro(valor_user,valor_em_porcetagem,True)
 
