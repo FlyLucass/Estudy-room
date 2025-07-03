3e9d36f5911a85835bc5b0dbe91590a206009e11
Lucas Santos <lucassantos30957@gmail.com>
2025-07-03 07:14:29 -0300

    FEAT: adicionado uma nova munção 'leia dinheiro', onde recebe parametros com entrada input onde verificando se os valores são monetários

--- Python/lucas python/UtilidadesCeV/moeda_value.py
+++ Python/lucas python/UtilidadesCeV/moeda_value.py
@@ -1,7 +1,32 @@
-def moeda(v):
+from . import moeda
+
+def moeda_formatada(v):
     formatado=f"R$: {v:.2f} "
     formatado=formatado.replace('.',',')
     return formatado
+
+def leiadinheiro(valor,porce,deci=False):
+    if valor.isnumeric()==True and porce.isnumeric()==True:
+        valor=float(valor)
+        porce=float(porce)
+        moeda.resumo(valor,porce,deci)
+    else:
+        print("Não foi digitado um valor monetário, em algum dos campos\n")
+        while valor.isnumeric()!=True or porce.isnumeric()!=True:
+            valor=input("Entre com um valor: ")
+            porce=input("Uma porcetagem a ser atribuida ao valores:")
+        valor=float(valor)
+        porce=float(porce)
+        moeda.resumo(valor,porce,deci)
+
+    
+        
+        
+        
+        
+    
+
+
 
 
    
