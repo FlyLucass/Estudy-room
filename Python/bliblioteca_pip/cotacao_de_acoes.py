4f58e21febd490518ae180d0885240d741d3ea39
Lucas Santos <163779057+FlyLucass@users.noreply.github.com>
2026-04-05 21:19:52 -0300

    últimas alterações

--- Python/bliblioteca_pip/cotacao_de_acoes.py
+++ Python/bliblioteca_pip/cotacao_de_acoes.py
@@ -1,3 +1,10 @@
-import requests;
-    
+import datetime
+from mercados.b3 import B3
+
+b3 = B3()
+negocios = b3.negociacao_bolsa("dia", datetime.date(2025, 9, 5))
+for negocio in negocios:
+    if negocio.codigo_negociacao in ("BBAS3", "PETR4", "BBSE3"):
+        print(negocio.codigo_negociacao, negocio.preco_abertura, negocio.preco_ultimo)
+
 
